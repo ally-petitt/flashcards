529140656e85f474fa2e54b7601034c623288c20
ally-petitt <allypetitt@gmail.com>
2021-04-10 18:32:03 -0700

    added create-card card

--- client/src/components/Decks/Decks.js
+++ client/src/components/Decks/Decks.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import AddIcon from "@material-ui/icons/Add";
+
 import Deck from "./Deck";
 
 function Decks() {
   return (
-    <div>
-      <h1>DECKS</h1>
+    <div className="p-3">
+      <div className="row w-100">
+        <Link to="/create" className="col-6 col-sm-4 col-md-3 mb-3">
+          <div
+            className="d-flex justify-content-center align-items-center bg-light rounded"
+            style={{ height: "200px" }}
+          >
+            <AddIcon fontSize="large" style={{ fill: "black" }}></AddIcon>
+          </div>
+        </Link>
+      </div>
       <Deck />
     </div>
   );
