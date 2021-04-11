33dfa27c0763f2cfdcc0cb3e8e8106eb6e47b3bc
ally-petitt <allypetitt@gmail.com>
2021-04-10 18:04:26 -0700

    created header and body with bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -10,10 +10,22 @@ import "./App.css";
 function App() {
   return (
     <Router>
-      <Route path="/" exact component={Decks} />
-      <Route path="/view/:id" component={Flashcards} />
-      <Route path="/edit/:id" component={EditCard} />
-      <Route path="/create" component={CreateCard} />
+      <div
+        className="wrapper bg-dark p-0 m-0"
+        style={{ minHeight: 100 + "vh" }}
+      >
+        <header className="p-3 mb-3 border-bottom border-secondary bg-dark">
+          <h1 className="h4 m-0">
+            <Link to="/" className="text-light">
+              RENAME
+            </Link>
+          </h1>
+        </header>
+        <Route path="/" exact component={Decks} />
+        <Route path="/view/:id" component={Flashcards} />
+        <Route path="/edit/:id" component={EditCard} />
+        <Route path="/create" component={CreateCard} />
+      </div>
     </Router>
   );
 }
