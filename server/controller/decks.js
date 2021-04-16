d44eea026c8c6a9d7fe77c1a0a59fd520639c003
ally-petitt <allypetitt@gmail.com>
2021-04-15 21:30:29 -0700

    Organized routes and controllers into separate folders

---
+++ server/controller/decks.js
@@ -0,0 +1,59 @@
+const Deck = require("../models/Deck");
+
+const getDecks = (req, res) => {
+  Deck.find((err, decks) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(decks);
+    }
+  });
+};
+
+const getDeck = (req, res) => {
+  let id = req.params.id;
+  Deck.findById(id, (err, deck) => {
+    res.json(deck);
+  });
+};
+
+const createDeck = (req, res) => {
+  let deck = new Deck(req.body);
+  deck
+    .save()
+    .then((deck) => {
+      res.status(200).json({ status: "deck added successfully" });
+    })
+    .catch((err) => {
+      res.status(400).send("adding new deck failed");
+    });
+};
+
+const updateDeck = (req, res) => {
+  Deck.findById(req.params.id, (err, deck) => {
+    if (!deck) {
+      res.status(404).send("deck is not found");
+    } else {
+      deck.deck_info = req.body.deck_info;
+      deck.deck_cards = req.body.deck_cards;
+
+      deck
+        .save()
+        .then((deck) => {
+          res.json("Deck Updated!");
+        })
+        .catch((err) => {
+          res.status.send(400).send("Update not possible");
+        });
+    }
+  });
+};
+
+const crudFunctions = {
+  getDecks: getDecks,
+  getDeck: getDeck,
+  createDeck: createDeck,
+  updateDeck: updateDeck,
+};
+
+module.exports = crudFunctions;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-const Deck = require("./models/Deck");
+const deckRoutes = require("./routes/decks");
 const app = express();
 
 const PORT = 5000;
@@ -9,6 +9,8 @@ const PORT = 5000;
 app.use(express.json({ type: "application/json" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+app.use("/decks", deckRoutes);
 
 app.get("/", (req, res) => {
   res.send("index page");
@@ -26,59 +28,4 @@ const connection = mongoose.connection;
 
 connection.once("open", function () {
   console.log("MongoDB database is connected!");
-});
-
-const deckRoutes = express.Router();
-app.use("/decks", deckRoutes);
-
-// TODO: after these routes are working, organize them in separate files to prevent server.js from getting to large
-
-deckRoutes.route("/").get((req, res) => {
-  Deck.find((err, decks) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.json(decks);
-    }
-  });
-});
-
-deckRoutes.route("/:id").get((req, res) => {
-  // it gets the id parameter from the route
-  let id = req.params.id;
-  Deck.findById(id, (err, deck) => {
-    res.json(deck);
-  });
-});
-
-deckRoutes.route("/add").post((req, res) => {
-  let deck = new Deck(req.body);
-  deck
-    .save()
-    .then((deck) => {
-      res.status(200).json({ status: "deck added successfully" });
-    })
-    .catch((err) => {
-      res.status(400).send("adding new deck failed");
-    });
-});
-
-deckRoutes.route("/update/:id").post((req, res) => {
-  Deck.findById(req.params.id, (err, deck) => {
-    if (!deck) {
-      res.status(404).send("deck is not found");
-    } else {
-      deck.deck_info = req.body.deck_info;
-      deck.deck_cards = req.body.deck_cards;
-
-      deck
-        .save()
-        .then((deck) => {
-          res.json("Deck Updated!");
-        })
-        .catch((err) => {
-          res.status.send(400).send("Update not possible");
-        });
-    }
-  });
 });
