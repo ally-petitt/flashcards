71256034c0134eb681e1eb359456004f61b36967
ally-petitt <allypetitt@gmail.com>
2021-04-18 21:16:45 -0700

    updated form in UI when delete button is pressed

--- client/src/components/Decks/EditDeck.js
+++ client/src/components/Decks/EditDeck.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router";
+import { Link } from "react-router-dom";
 
 function EditDeck() {
   const [deck, setDeck] = useState({
@@ -48,57 +49,91 @@ function EditDeck() {
     }
   };
 
+  const handleDelete = () => {
+    console.log("delete");
+    // confirm that they would like to delete
+    if (window.confirm("Are you sure you want to delete this deck?")) {
+      // show that deck was deleted
+      document.getElementById("after-delete").classList.remove("d-none");
+      document.getElementById("after-delete").classList.add("d-block");
+      document.getElementById("delete-notif").classList.add("d-flex");
+
+      // remove buttons
+      const buttons = document.getElementsByClassName("btn");
+      for (const button of buttons) {
+        button.classList.add("d-none");
+      }
+    }
+  };
+
   return (
     <div className="create-deck_container d-flex justify-content-center align-items-center text-center p-5">
-      <form
-        onSubmit={handleSubmit}
+      <div
         className="bg-light rounded w-75 h-75 p-3 p-sm-4 p-md-5"
-        id="form"
         style={{ maxWidth: "800px" }}
       >
-        <h3>EDIT DECK</h3>
-        <label htmlFor="title" className="float-left">
-          Title <span className="text-danger">*</span>
-        </label>
-        <input
-          className="form-control form-control-lg my-2"
-          type="text"
-          id="title"
-          defaultValue={deck.deck_info.title}
-          onChange={(e) =>
-            setDeck({
-              ...deck,
-              deck_info: { ...deck.deck_info, title: e.target.value },
-            })
-          }
-        />
-        <p
-          className="text-danger float-left m-0 mb-2 d-none"
-          id="required-message"
-        >
-          The title is required
-        </p>
-        <br />
-        <label htmlFor="description" className="float-left text-left w-100">
-          Description
-        </label>
-        <textarea
-          className="form-control my-3"
-          type="text"
-          rows="5"
-          id="description"
-          defaultValue={deck.deck_info.description}
-          onChange={(e) =>
-            setDeck({
-              ...deck,
-              deck_info: { ...deck.deck_info, description: e.target.value },
-            })
-          }
-        ></textarea>
-        <button type="submit" className="btn btn-outline-dark mt-3">
-          Save
+        <form onSubmit={handleSubmit} id="form">
+          <h3>EDIT DECK</h3>
+          <label htmlFor="title" className="float-left">
+            Title <span className="text-danger">*</span>
+          </label>
+          <input
+            className="form-control form-control-lg my-2"
+            type="text"
+            id="title"
+            defaultValue={deck.deck_info.title}
+            onChange={(e) =>
+              setDeck({
+                ...deck,
+                deck_info: { ...deck.deck_info, title: e.target.value },
+              })
+            }
+          />
+          <p
+            className="text-danger float-left m-0 mb-2 d-none"
+            id="required-message"
+          >
+            The title is required
+          </p>
+          <br />
+          <label htmlFor="description" className="float-left text-left w-100">
+            Description
+          </label>
+          <textarea
+            className="form-control my-3"
+            type="text"
+            rows="5"
+            id="description"
+            defaultValue={deck.deck_info.description}
+            onChange={(e) =>
+              setDeck({
+                ...deck,
+                deck_info: { ...deck.deck_info, description: e.target.value },
+              })
+            }
+          ></textarea>
+          <button type="submit" className="btn btn-outline-dark my-3">
+            Save
+          </button>
+        </form>
+        <button className="btn btn-outline-danger" onClick={handleDelete}>
+          Delete
         </button>
-      </form>
+        <div id="after-delete" className="w-75 mx-auto mt-4 d-none">
+          <div
+            className="border border-danger text-danger w-100 align-items-center justify-content-center rounded mb-3"
+            style={{
+              height: "50px",
+              backgroundColor: "rgba(220, 53, 69, 0.3)",
+              borderWidth: "3px",
+            }}
+            id="delete-notif"
+          >
+            <p className="p-1 m-0">Deck was deleted</p>
+          </div>
+          <Link to="/home">Return home</Link>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ client/src/components/Warning.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Warning() {
+  return (
+    <div>
+      <h1>WARNING</h1>
+    </div>
+  );
+}
+
+export default Warning;
