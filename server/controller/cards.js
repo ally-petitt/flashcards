d9a94bbdba0a0f4f9e0ad1f7fcdf4b01ce7f4215
ally-petitt <allypetitt@gmail.com>
2021-04-20 22:13:55 -0700

    Created backend for flashcards!

--- server/controller/cards.js
+++ server/controller/cards.js
@@ -6,13 +6,12 @@ const createCard= (req, res) => {
     // create a new model with information
     const card = new Card({ card_info: req.body});
 
-    
+    card.save().then(() => res.json("Card created!")).catch(() => res.json("Could not create card"))
     // add id to deck "cards" array
     Deck.findById(req.params.deck_id, (err, deck) => {
         if (!deck) {
             res.status(404).send("Deck not found")
         }
-        // makes it faster to find cooresponding cards
         deck.cards.push(card._id)
         deck.save().then(() => res.status(200).send("Id added to deck!")).catch(err => res.json("Could not add card to deck"))
     })
