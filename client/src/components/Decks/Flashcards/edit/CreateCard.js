c51abccf459d26c6518dedd6515bfc3e2fb81ec4
ally-petitt <allypetitt@gmail.com>
2021-04-23 16:25:11 -0700

    Created setup for post request in form component

--- client/src/components/Decks/Flashcards/edit/CreateCard.js
+++ client/src/components/Decks/Flashcards/edit/CreateCard.js
@@ -1,43 +1,35 @@
 import React, { useState } from 'react';
 import axios from "axios";
 import Form from "./Form"
+import { useParams } from 'react-router';
 
 function CreateCard() {
     const [cardData, setCardData] = useState({
-    title: "",
-    description: "",
-    color: "#ffffff",
-    isStarred: false,
-  });
+        front: "",
+        back: ""
+    });
 
-  const [wasSuccessful, setWasSuccessful] = useState()
+    const [wasSuccessful, setWasSuccessful] = useState()
 
-    let deckId = 17
+    const { deck_id } = useParams();
 
     const storeData = () => {
-        const newDeck = {
-      deck_info: {
-        title: cardData.title,
-        description: cardData.description,
-        color: cardData.color,
-      },
-    };
+        const newCard = {
+            card_info: {
+                front: cardData.front,
+                back: cardData.back
+            },
+        };
 
-    // send post request
-    axios.post("http://localhost:4000/decks/create", newDeck).then(
-      (res) => setWasSuccessful(true))
-      .catch((err) =>setWasSuccessful(false)
-    );
-    // reset state
-    setCardData({ 
-      title: "",
-      description: "",
-      color: "#ffffff",
-    });
+        // send post request
+        axios.post(`http://localhost:4000/decks/${deck_id}/cards/create`, newCard).then(
+            (res) => setWasSuccessful(true))
+            .catch((err) =>setWasSuccessful(false)
+        );
     }
     return (
         <div>
-            <Form title="CREATE A NEW CARD" btnText="Create Card" deckId={deckId} storeData={storeData} state={cardData} setState={setCardData} wasSuccessful={wasSuccessful} />
+            <Form title="CREATE A NEW CARD" btnText="Create Card" deckId={deck_id} storeData={storeData} state={cardData} setState={setCardData} wasSuccessful={wasSuccessful} />
         </div>
     )
 }
