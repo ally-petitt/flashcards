9782b5351e9aa2bd41b0edf1e31bb6795c673ab6
ally-petitt <allypetitt@gmail.com>
2021-04-26 21:02:10 -0700

    Created style for review component

--- client/src/components/Decks/Deck/review/Review.js
+++ client/src/components/Decks/Deck/review/Review.js
@@ -1,10 +1,34 @@
+import { Link } from "@material-ui/core";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
 import React from "react";
+import { useParams } from "react-router";
 
 function Review() {
+  const { id } = useParams();
+
   return (
-    <div>
-      <h1>REVIEWASDFASDF</h1>
-    </div>
+    <>
+      <Link
+        to={`/deck/view/${id}`}
+        style={{ color: "white", marginLeft: "22px" }}
+      >
+        <ChevronLeftIcon fontSize="large" />
+      </Link>
+      <div
+        className="container d-flex justify-content-center align-items-center position-absolute flex-column text-center text-light"
+        style={{
+          height: "100vh",
+          width: "100vw",
+          top: "0",
+          left: "0",
+          right: "0",
+        }}
+      >
+        <p className="h2">Front</p>
+        <button className="btn btn-outline-light mt-4">Flip</button>
+        <button className="btn btn-outline-success mt-4">Next Card</button>
+      </div>
+    </>
   );
 }
 
